020eacbf1894358a3231b0d1ccf902d0b9b36104
Thom <git@thomi.nl>
2015-10-28 15:25:04 +0100

    Minor bug variable mismatch

--- signal433driver.h
+++ signal433driver.h
@@ -7,7 +7,7 @@
 //Software PWM Pin(GPIO0-11)
 
 extern int signal433Create(int pin);
-extern int  __attribute__ ((hot)) signal433Send(sendData_t toSend);
+extern int  __attribute__ ((hot)) signal433Send(char *toSend);
 extern void signal433Stop();
 
 
